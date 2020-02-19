f873752a43f06e759f9d3bcc2f4d4ba4a57d706e
unknown <eloso85@gmail.com>
2020-02-18 19:24:48 -0600

    added api

--- assets/java/app.js
+++ assets/java/app.js
@@ -6,8 +6,21 @@ window.addEventListener('load', ()=>{
         navigator.geolocation.getCurrentPosition(position =>{
             long = position.coords.longitude;
             lat = position.coords.latitude
+        
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6d19623049abd3ae1e49417461fb8938`
+
+            fetch (api)
+            .then (response =>{
+                return response.json();
+            })
+            .then (data =>{
+                console.log(data)
+            });
         });
-    }else{
-        h1.textContent ="Please allow location access"
-    }
+        
+      
+    
+        }
+
+    
 });
