24937dff0bdad4f58ea86f96c51211c05306ed0b
unknown <eloso85@gmail.com>
2020-02-20 10:31:12 -0600

    weather is working

--- assets/java/app.js
+++ assets/java/app.js
@@ -10,7 +10,7 @@ window.addEventListener('load', ()=>{
             long = position.coords.longitude;
             lat = position.coords.latitude
         
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6d19623049abd3ae1e49417461fb8938`
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=6d19623049abd3ae1e49417461fb8938`
 
             fetch (api)
             .then (response =>{
@@ -18,12 +18,21 @@ window.addEventListener('load', ()=>{
             })
             .then (data =>{
                 console.log(data)
-                const {temp} = data.main;
-                const {description} = data.weather;
+                // $(".temperature-description").html(`
+                // ${data.weather[0].map(function(player){
+                //     return`
+                //     ${data.main}
+                //     `
+                // }).join('')}
+                // `)
+                const temp = data.main;
+                const weather = data.weather[0].main;
+                const location = data.name;
                 //set Dom Elements from api
                 temperatureDegree.textContent = temp;
-                temperatureDescription.textContent = description;
-                console.log(description);
+                temperatureDescription.textContent = weather;
+                locationTimezone.textContent = location;
+                console.log(temp);
             });
         });
         
